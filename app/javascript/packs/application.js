fa3a80934a0a062ebe6801f5b6b8cea0386838dc
Richard Schmidt <richardnew12511@gmail.com>
2021-06-07 12:22:53 -0500

    add fontawesome via yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import "../stylesheets/application"
 // require("stylesheets/application.scss")
+import "@fortawesome/fontawesome-free/css/all"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
