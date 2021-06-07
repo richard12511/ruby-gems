8587a91b7a9c86dc1ac151e2e5e76a6edb2e0538
Richard Schmidt <richardnew12511@gmail.com>
2021-06-07 14:10:24 -0500

    datatables working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,20 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('datatables.net-bs4')
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import "../stylesheets/application"
-// require("stylesheets/application.scss")
-import "@fortawesome/fontawesome-free/css/all"
+require("stylesheets/application.scss")
+// import "@fortawesome/fontawesome-free/css/all"
+
+import $ from 'jquery'
+global.$ = jQuery;
+
+// Add DataTables jQuery plugin
+// require('imports-loader?define=>false!datatables.net')(window, $)
+// require('imports-loader?define=>false!datatables.net-select')(window, $)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
